bfc21fa02107f0f3089d64df2a568ab66a9e3fd2
Florent Kolosowski <fkolosowski@ivsweb.com>
2015-05-20 17:53:41 +0200

    linterface launght the stream (wip)

--- stream-server.js
+++ stream-server.js
@@ -5,21 +5,15 @@ var Streamer = require('./lib/streamer.js'),
     args     = process.parseargs().dict,
     server;
 
-//interface = new Interface();
-
 Streamer.listDevices(function(devices){
    console.info("Available video devices", devices.video);
   if(!devices.video.length)
     throw "Cannot find any video device";
   if(!args.camera_name) {
-      args.camera_name = devices.video[0]
-      console.info("Using default camera ",args.camera_name);
-   } else if(!devices.video.contains(args.camera_name))
-      throw ("Invalid camera name " + args.camera_name);
-
-  server = new Streamer(args);
-  server.startServer(function(){
-    server.startStreamer();
-  });
-
+    args.camera_name = devices.video[0]
+    console.info("Using default camera ",args.camera_name);
+  } else if(!devices.video.contains(args.camera_name))
+    throw ("Invalid camera name " + args.camera_name);
+  Streamer.devices = devices;
+  interface = new Interface(Streamer);
 });
